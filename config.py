a0cf65b6618304868ec81c43a6184bc51fe54578
abidbaig177 <119119875+abidbaig177@users.noreply.github.com>
2023-03-19 23:00:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,8 +9,8 @@ BOT_TOKEN = getenv("5921061038:AAHjBKykr_DEij3EvpLAyaZJsXidt1syhyk")
 
 MONGO_DB_URI = getenv("mongodb+srv://TELEGRAM:TELEGRAM@cluster0.tznsnwb.mongodb.net/?retryWrites=true&w=majority")
 
-INDEX_ID = int(getenv("INDEX_ID"))
-UPLOADS_ID = int(getenv("UPLOADS_ID"))
+INDEX_ID = int(getenv("-1001963941283"))
+UPLOADS_ID = int(getenv("-1001909816933"))
 
 STATUS_ID = int(getenv("STATUS_ID"))
 SCHEDULE_ID = int(getenv("SCHEDULE_ID"))
