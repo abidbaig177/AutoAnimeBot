fb8b1c0a53e727ae9b5295fd1b73a0587362dc99
abidbaig177 <119119875+abidbaig177@users.noreply.github.com>
2023-03-19 22:50:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-BOT_TOKEN = getenv("BOT_TOKEN")
+API_ID = int(getenv("23864794"))
+API_HASH = getenv("0176a32f396e45a0a33644f678507d76")
+BOT_TOKEN = getenv("5921061038:AAHjBKykr_DEij3EvpLAyaZJsXidt1syhyk")
 
-MONGO_DB_URI = getenv("MONGO_DB_URI")
+MONGO_DB_URI = getenv("mongodb+srv://TELEGRAM:TELEGRAM@cluster0.tznsnwb.mongodb.net/?retryWrites=true&w=majority")
 
 INDEX_ID = int(getenv("INDEX_ID"))
 UPLOADS_ID = int(getenv("UPLOADS_ID"))
